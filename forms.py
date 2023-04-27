3ef1085ca275c655c5b0d35a8d1affaed1073ecb
Yoban Nova <73599932+ysnovaa@users.noreply.github.com>
2023-04-27 01:06:20 -0500

    Organización

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import conductores
-
-class ConductorForm(ModelForm):
-    class Meta:
-        model = conductores
-        fields = ['documento', 'nombre_completo', 'edad', 'tipo_licencia', 'bus_asignado']
